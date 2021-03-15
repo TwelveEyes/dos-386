8ce7c879540d877180ee69d1d0d6be799c6b2c2e
gene <softhaxorz@gmail.com>
2021-03-14 22:21:49 -0700

    tidy up fullscreen code

--- public/js-misc/js-fullscreen.js
+++ public/js-misc/js-fullscreen.js
@@ -1,9 +1,11 @@
 const fullscreen = () => {
-  var el = document.getElementById("jsdos");
+  const el = document.getElementById("jsdos");
 
-  if (el.webkitRequestFullScreen) {
-    el.webkitRequestFullScreen();
-  } else {
-    el.mozRequestFullScreen();
-  }
+  if (el.requestFullscreen) el.requestFullscreen();
+  // Firefox
+  else if (el.mozRequestFullScreen) el.mozRequestFullScreen();
+  // Chrome, Safari
+  else if (el.webkitRequestFullScreen) el.webkitRequestFullScreen();
+  // IE 11, Edge
+  else if (el.msRequestFullscreen) el.msRequestFullscreen();
 };
